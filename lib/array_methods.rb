a601a2952f9a19f05e8ba70a048a97e274d080e2
Treven Womack <keytonwomack@yahoo.com>
2019-11-18 10:03:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,14 +3,13 @@ array.length.times do |count|
   if array[count] == value_to_find
     return count
   end
-  nil
 end
-
+nil
 end
 
 def find_max_value(array)
   array.length.times do |count|
-    
+    x= array[0]
 
 def find_min_value(array)
   # Add your solution here
