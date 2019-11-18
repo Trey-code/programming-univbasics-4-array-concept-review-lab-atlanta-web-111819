db7356b0ca2a029e13e83e700d93c733b85bdb94
Treven Womack <keytonwomack@yahoo.com>
2019-11-18 10:06:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,8 +8,14 @@ nil
 end
 
 def find_max_value(array)
-  
-  array.length.times do |count|
+  x= array[0]
+  array.length.times { |index|
+  if array[index] > x 
+    x = array[index]
+  end
+  }
+  x 
+end
     
 
 def find_min_value(array)
